2352120c04714c8371d216a1b6658bbcc5ed56bb
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2020-11-30 12:38:40 +0200

    Add configuration

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  makeIncrementer = (amount) => () =>
+    this.setState((prevState) => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  decrement = this.makeIncrementer(-1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>
+          Increment
+        </button>
+        <button className="decrement" onClick={this.decrement}>
+          Decrement
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  it("starts with a count of 0", () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: 0");
+  });
+
+  it("increments count by 1 when the increment button is clicked", () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find("button.increment");
+    incrementBtn.simulate("click");
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: 1");
+  });
+
+  it("decrements count by 1 when the decrement button is clicked", () => {
+    const wrapper = shallow(<App />);
+    const decrementBtn = wrapper.find("button.decrement");
+    decrementBtn.simulate("click");
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: -1");
+  });
+
+  it("matches the snapshot", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
